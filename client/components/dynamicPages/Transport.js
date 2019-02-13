453d524e678af06856938d608a67f45af8c5d35f
aterna01 <ssaniok90@mail.ru>
2019-02-13 12:27:23 +0000

    add class component

---
+++ client/components/dynamicPages/Transport.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Transport(props) {
+    return (
+        <div>
+            <h1>Transport</h1>
+        </div>
+    );
+}
+
+export default Transport;
---
+++ client/components/dynamicPages/Work.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Work(props) {
+    return (
+        <div>
+            <h1>Work</h1>
+        </div>
+    );
+}
+
+export default Work;
