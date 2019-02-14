99d0411f1a6fa5b5dc4afb7aeb109c91b59f874f
Kristina Jaggard <jaggardk@gmail.com>
2019-02-14 16:17:33 +0000

    update state to include input from all questions

--- client/components/dynamicPages/Toilet.js
+++ client/components/dynamicPages/Toilet.js
@@ -13,7 +13,7 @@ function Toilet(props) {
                     <label>Do you need help to use the toilet? </label>
                     <input type="checkbox"></input></li>
                 <li>
-                    <label>Do you need help to use the toilet?</label>
+                    <label> Do you need help to go to the toilet at night</label>
                     <input type="checkbox"></input></li>
             </ul>
             <label>The questions above are examples of how you may meet this requirement. Please expand and / or let us know other ways that you need help managing toilet needs.</label>
