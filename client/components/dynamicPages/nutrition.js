d383197e6d5db43016f98c435ae0445e31b6ab18
Kristina Jaggard <jaggardk@gmail.com>
2019-02-14 12:42:59 +0000

    lost files of merge

--- client/components/dynamicPages/nutrition.js
+++ client/components/dynamicPages/nutrition.js
@@ -1,41 +1,11 @@
 import React from 'react';
 
-function QuizOne(props) {
-
-    const displayNutrition = Object.keys(props.info).map((qs, i) =>
-    {if(qs === "more") {
-        return (
-            <div>
-                <p>Tell us more</p>
-                <textarea type="text"
-                    name="more"
-                    onChange={(event) => props.handleChangeInput(event, qs, props.section)}
-                />
-            </div>
-        )
-    }else {
-        return (
-            <div>
-                <h2>{props.info[qs].question}</h2>
-                <input type="checkbox"
-                    checked={qs.agree}
-                    name="agree"
-                    key={`question${i}`}
-                    onChange={(event) => props.toggleChange(event, qs, props.section)}
-                />
-            </div>
-        )
-    }}
-
-    )
-
-    return(
+function Nutrion(props) {
+    return (
         <div>
-            <h1>Nutrition is working</h1>
-            {displayNutrition}
-
+            <h1>Nutrition</h1>
         </div>
-    )
+    );
 }
 
-export default QuizOne;
+export default Nutrion;
