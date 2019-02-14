23247a0ca0af93080b61498f1df4acc3f219a355
aterna01 <ssaniok90@mail.ru>
2019-02-14 14:23:11 +0000

    fix about route

--- client/components/staticPages/About.js
+++ client/components/staticPages/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../images/logo.png';
 
 function About() {
@@ -8,15 +9,19 @@ function About() {
             <Link to={'./menu'}>
                 <button>Menu</button>
             </Link>
-            <img src={ logo } class="logo"></img>
+            <img src={logo} class="logo" />
             <h1>About us</h1>
-            <p>The Advocacy Project has developed this app to help people 
-                with the process of getting the help they need from the local authority. 
-                <br/>
-                <br/>
+            <p>
+                The Advocacy Project has developed this app to help people with
+                the process of getting the help they need from the local
+                authority.
+                <br />
+                <br />
                 For more information, check out&nbsp;
-                <a href="https://www.advocacyproject.org.uk/news/the-advocacy-project-in-hackney/">our website.</a>
-                </p>
+                <a href="https://www.advocacyproject.org.uk/news/the-advocacy-project-in-hackney/">
+                    our website.
+                </a>
+            </p>
         </div>
     );
 }
