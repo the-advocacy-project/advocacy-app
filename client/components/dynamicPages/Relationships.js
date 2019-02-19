551696dbf813f01a2e8ce6902bd757d96a9bfbde
Kristina Jaggard <jaggardk@gmail.com>
2019-02-19 22:42:57 +0000

    add text input func to outcomes questions

--- client/components/dynamicPages/Relationships.js
+++ client/components/dynamicPages/Relationships.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 import NavHeaderForm from '../navigation/NavHeaderForm';
 
 function Relationships(props) {
-    const displayNutrition = Object.keys(props.info).map((qs, i) => {
+    const  displayRelationships  = Object.keys(props.info).map((qs, i) => {
         if (qs === 'more') {
-            console.log('I am textarea');
+
             return (
                 <div>
                     <label>
@@ -16,26 +16,22 @@ function Relationships(props) {
                         family or personal relationships.
                     </label>
                     <div className="textAreaInput">
-                        <p className="textAreaText">Tell us more</p>
                         <textarea
+                            placeholder="Tell us more..."
                             className="textArea more__questions"
                             type="text"
                             name="more"
                             key={i}
-                            onChange={event =>
-                                props.handleChangeInput(
-                                    event,
-                                    qs,
-                                    props.section
-                                )
-                            }
+                            onChange={event => {
+                                event.preventDefault();
+                                props.handleChangeInput(event, qs, props.section)
+                            }}
                         />
                     </div>
                 </div>
             );
         } else {
-            console.log('I am a checkbox');
-            console.log('checked: ', props.info[qs].agree);
+
             return (
                 <div className="checkboxInput">
                     <h2 className="checkboxText">{props.info[qs].question}</h2>
@@ -63,12 +59,11 @@ function Relationships(props) {
                     relationships
                 </h1>
             </NavHeaderForm>
-            {/* <h1 className="nutritionH1">Managing and Maintaining Nutrition</h1> */}
             <div className="wrapper">
                 <h2 className="margin50 h2__questions">
                     Please check what applies to you
                 </h2>
-                {displayNutrition}
+                { displayRelationships }
                 <div className="margin50 clear" />
             </div>
             <div className="navButtons">
