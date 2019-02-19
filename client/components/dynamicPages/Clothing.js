960f959e46102849b05ecae73ae3ba7a2a6611ee
aterna01 <ssaniok90@mail.ru>
2019-02-19 13:42:36 +0000

    form pages

--- client/components/dynamicPages/Clothing.js
+++ client/components/dynamicPages/Clothing.js
@@ -1,36 +1,92 @@
 import React from 'react';
-import {Button} from '../buttons/Button';
+import { Button } from '../buttons/Button';
 import { Link } from 'react-router-dom';
 import NavHeaderForm from '../navigation/NavHeaderForm';
+import '../../styles/css/styles.css';
 
 function Clothing(props) {
+    const displayNutrition = Object.keys(props.info).map((qs, i) => {
+        if (qs === 'more') {
+            console.log('I am textarea');
+            return (
+                <div>
+                    <label>
+                        The questions above are examples of how you may meet
+                        this requirement. Please expand and / or let us know
+                        other ways that you need help being appropriately
+                        clothed.
+                    </label>
+                    <div className="textAreaInput">
+                        <p className="textAreaText">Tell us more</p>
+                        <textarea
+                            className="textArea more__questions"
+                            type="text"
+                            name="more"
+                            key={i}
+                            onChange={event =>
+                                props.handleChangeInput(
+                                    event,
+                                    qs,
+                                    props.section
+                                )
+                            }
+                        />
+                    </div>
+                </div>
+            );
+        } else {
+            console.log('I am a checkbox');
+            console.log('checked: ', props.info[qs].agree);
+            return (
+                <div className="checkboxInput">
+                    <h2 className="checkboxText">{props.info[qs].question}</h2>
+                    <input
+                        className="checkBox"
+                        type="checkbox"
+                        checked={props.info[qs].agree}
+                        name="agree"
+                        key={i}
+                        onChange={event =>
+                            props.toggleChange(event, qs, props.section)
+                        }
+                    />
+                    <div className="margin50" />
+                </div>
+            );
+        }
+    });
+
     return (
         <div>
-            <NavHeaderForm />
-            <h1>Being appropriately clothed</h1>
-            <h2>Please check what applies to you</h2>
-            <ul>
-                <li><label>Do you need help to put your clothes and shoes on in the morning?</label> <input type="checkbox"></input></li>
-                <li><label>Do you need help to take your clothes and shoes off?</label> <input type="checkbox"></input></li>
-                <li><label>Do you need help to buy new clothes for different occasions such as work?</label> <input type="checkbox"></input></li>
-            </ul>
-            <label>The questions above are examples of how you may meet this requirement. Please expand and / or let us know other ways that you need help being appropriately clothed.</label>
-            <textarea placeholder="Tell us more..."></textarea>
-            <div className="navButtons"><Link to={'./toilet'} >
-                <Button 
-                text='Back' 
-                type='submit' 
-                className="buttons"
-                onClick={() => {}}/>
-            </Link>
-            <Link to={'./safety'} >
-                <Button 
-                text='Next' 
-                type='submit' 
-                className="buttons"
-                onClick={() => {}}/>
-            </Link>
-        </div>
+            <NavHeaderForm>
+                <h1 className="h1__questions">Being appropriately clothed</h1>
+            </NavHeaderForm>
+            {/* <h1 className="nutritionH1">Managing and Maintaining Nutrition</h1> */}
+            <div className="wrapper">
+                <h2 className="margin50 h2__questions">
+                    Please check what applies to you
+                </h2>
+                {displayNutrition}
+                <div className="margin50 clear" />
+            </div>
+            <div className="navButtons">
+                <Link to={'./toilet'}>
+                    <Button
+                        text="Back"
+                        type="submit"
+                        className="buttons"
+                        onClick={() => {}}
+                    />
+                </Link>
+                <Link to={'./safety'}>
+                    <Button
+                        text="Next"
+                        type="submit"
+                        className="buttons"
+                        onClick={() => {}}
+                    />
+                </Link>
+            </div>
         </div>
     );
 }
--- client/components/dynamicPages/Eligibility.js
+++ client/components/dynamicPages/Eligibility.js
@@ -1,33 +1,91 @@
 import React from 'react';
-import {Button} from '../buttons/Button';
+import { Button } from '../buttons/Button';
 import { Link } from 'react-router-dom';
 import NavHeader from '../navigation/navHeader';
 import '../../styles/css/home.css';
 
-
 function Eligibility(props) {
+    const displayNutrition = Object.keys(props.info).map((qs, i) => {
+        if (qs === 'more') {
+            console.log('I am textarea');
+            return (
+                <div>
+                    <label>
+                        This includes if you have a condition as a result of
+                        physical, mental, sensory, learning or cognitive
+                        disabilities or illnesses, substance misuse or brain
+                        injury.
+                    </label>
+                    <div className="textAreaInput">
+                        <p className="textAreaText">Tell us more</p>
+                        <textarea
+                            className="textArea more__questions"
+                            type="text"
+                            name="more"
+                            key={i}
+                            onChange={event =>
+                                props.handleChangeInput(
+                                    event,
+                                    qs,
+                                    props.section
+                                )
+                            }
+                        />
+                    </div>
+                </div>
+            );
+        } else {
+            console.log('I am a checkbox');
+            console.log('checked: ', props.info[qs].agree);
+            return (
+                <div className="checkboxInput">
+                    <h2 className="checkboxText">{props.info[qs].question}</h2>
+                    <input
+                        className="checkBox"
+                        type="checkbox"
+                        checked={props.info[qs].agree}
+                        name="agree"
+                        key={i}
+                        onChange={event =>
+                            props.toggleChange(event, qs, props.section)
+                        }
+                    />
+                    <div className="margin50" />
+                </div>
+            );
+        }
+    });
+
     return (
         <div>
             <NavHeader />
-            <h1>Eligibility</h1>
-            <label>Does your need for support arise from a physical or mental impairment or illness?</label>
-            <p>This includes if you have a condition as a result of physical, mental, sensory, learning or cognitive disabilities or illnesses, substance misuse or brain injury.</p>
-            <input type="checkbox"></input>
-            <div className="navButtons"><Link to={'./responsibilites'} >
-                <Button 
-                text='Back' 
-                type='submit' 
-                className="buttons"
-                onClick={() => {}}/>
-            </Link>
-            <Link to={'./wellbeing'} >
-                <Button 
-                text='Next' 
-                type='submit' 
-                className="buttons"
-                onClick={() => {}}/>
-            </Link>
-        </div>
+
+            <div className="wrapper">
+                <h1>Eligibility</h1>
+                <h2 className="margin50 h2__questions">
+                    Please check what applies to you
+                </h2>
+                {displayNutrition}
+                <div className="margin50 clear" />
+            </div>
+            <div className="navButtons">
+                <Link to={'./responsibilites'}>
+                    <Button
+                        text="Back"
+                        type="submit"
+                        className="buttons"
+                        onClick={() => {}}
+                    />
+                </Link>
+                <Link to={'./wellbeing'}>
+                    <Button
+                        text="Next"
+                        type="submit"
+                        className="buttons"
+                        onClick={() => {}}
+                    />
+                </Link>
+            </div>
         </div>
     );
 }
