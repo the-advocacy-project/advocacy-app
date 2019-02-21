610c9f594cd0a111dc7a140d82e6f39ad11b6861
Kristina Jaggard <jaggardk@gmail.com>
2019-02-21 12:47:23 +0000

    set up sorry checks page

---
+++ client/components/staticPages/SorryChecks.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import NavHeader from '../navigation/navHeader';
+import '../../styles/css/how.css';
+import { Link } from 'react-router-dom';
+import {Button} from '../buttons/Button';
+
+function SorryChecks(){
+    return(
+        <div>
+            <NavHeader />
+            <div className="wrapper">
+                <h1>Sorry!</h1>
+                <p>You need to be 18 years of age or older and a resident of Hackney in order to apply for support under the Care Act 2014.</p>
+
+                
+                <div className="navButtons">
+                    <Link to={'./initial-checks'} >
+                        <Button 
+                            text='Back' 
+                            type='submit' 
+                            className="buttons"
+                            onClick={() => {}}/>
+                    </Link> 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SorryChecks;
