9fa24c371c66710a1990e770250b92c9f396bbb2
Cynthia Onwordi <onwordi.designs@gmail.com>
2019-03-01 11:57:15 +0000

    serving static files from client instead of dist

--- server/index.js
+++ server/index.js
@@ -346,7 +346,7 @@ app.post('/', (req, res) => {
     // res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
-app.use(express.static('dist'));
+app.use(express.static('client'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/../client/index.html'));
