b85ee284cc1a4db916950060109acab53b3a3d5c
Kristina Jaggard <jaggardk@gmail.com>
2019-03-01 11:03:04 +0000

    remove navheader from 404

--- client/components/staticPages/FourZeroFour.js
+++ client/components/staticPages/FourZeroFour.js
@@ -1,13 +1,13 @@
 import React from 'react';
-// import people from '../../images/people.png';
-import NavHeader from '../navigation/NavHeader';
+
+
 import '../../styles/css/home.css';
 
 function FourZeroFour(){
     return(
         <div id="outer-container">
             {window.scrollTo(0,0)}
-            <NavHeader />
+           
             <div id="page-wrap" className="wrapper">
                 <h1>Whoops, there's been an error</h1>
                 <h2>404</h2>
