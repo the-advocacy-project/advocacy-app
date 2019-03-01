2333319bb5d1570228e62a4fb0703ec371cb4c2d
Kristina Jaggard <jaggardk@gmail.com>
2019-03-01 11:05:14 +0000

    Navheader reimport into 404

--- client/components/staticPages/FourZeroFour.js
+++ client/components/staticPages/FourZeroFour.js
@@ -1,13 +1,12 @@
 import React from 'react';
-
-
+import NavHeader from '../navigation/NavHeader'
 import '../../styles/css/home.css';
 
 function FourZeroFour(){
     return(
         <div id="outer-container">
             {window.scrollTo(0,0)}
-           
+            <NavHeader />
             <div id="page-wrap" className="wrapper">
                 <h1>Whoops, there's been an error</h1>
                 <h2>404</h2>
