6f55f2847f07d6ff40a57346fc615d50332a6021
Kristina Jaggard <jaggardk@gmail.com>
2019-03-01 11:06:36 +0000

    update BurgerMenu capitalisation

--- client/components/navigation/navHeader.js
+++ client/components/navigation/navHeader.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import BurgerMenu from './burgerMenu';
+import BurgerMenu from './BurgerMenu';
 
 import '../../styles/css/navigation.css';
 
 const NavHeader = () => (
     <div>
-    <BurgerMenu pageWrapId={"page-wrap"} outerContainerId={"App"} />
-    <div className="nav-container">
-    <div className="nav-container--white"></div>
-    </div>
+        <BurgerMenu pageWrapId={"page-wrap"} outerContainerId={"App"} />
+        <div className="nav-container">
+        <div className="nav-container--white"></div>
+        </div>
     </div>
 );
 
